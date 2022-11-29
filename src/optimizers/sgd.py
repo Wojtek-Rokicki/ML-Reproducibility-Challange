c5da8c131d90eacbb056cda6c6aeb7d5be9eacd0
bedkowski.patrick <bedkowskisite@gmail.com>
2022-11-29 21:27:06 +0100

    Added SGD method

--- src/optimizers/sgd.py
+++ src/optimizers/sgd.py
@@ -1,9 +1,29 @@
-from src.logistic_regression.gradient import gradient
 import numpy as np
 
+from src.logistic_regression.log_reg_gradient import log_reg_gradient
+from src.logistic_regression.stochastic_gradient import stochastic_gradient
 
-def sgd(y, tx, w):
-    sample_index = np.random.randint(0, len(y))
-    y_sgd = y[sample_index]
-    x_sgd = np.array(tx[sample_index, :])
-    return gradient(y_sgd, x_sgd, w)
+
+def sgd(w_0, max_iter, tx, y, params):
+    """
+    Compute spider
+    :param w_0: initial weights vector.
+    :param max_iter:
+    :param tx:
+    :param y:
+    :param gamma: Step size
+    :return:
+    """
+    gamma = params.gamma
+    grads = []
+    w = [w_0]
+    n = len(y)
+
+    for t in range(max_iter):
+        i_t = np.random.choice(np.arange(1, n))  # get index of sample for which to compute gradient
+        gradient = stochastic_gradient(y, tx, w[t], [i_t])
+        w_next = w[t] - gamma*gradient
+
+        w.append(w_next)
+        grads.append(gradient)
+    return grads
