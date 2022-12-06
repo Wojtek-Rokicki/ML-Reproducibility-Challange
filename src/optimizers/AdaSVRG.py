c09e420fad42e703f28064f994251f370380acdc
IsaacBattles <49080381+IsaacBattles@users.noreply.github.com>
2022-12-06 19:17:10 +0100

    fixed AdaSVRG and SVRG

--- src/optimizers/AdaSVRG.py
+++ src/optimizers/AdaSVRG.py
@@ -4,26 +4,29 @@ from src.logistic_regression.stochastic_gradient import stochastic_gradient
 from src.utils.method_utils import *
 
 
-def AdaSVRG(w_0, tx, y, K, m):
+def AdaSVRG(w_0, tx, y, max_iter, q, lamb):
     w = [w_0]
-    step_size = 1 / np.max([np.linalg.norm(tx[i])**2 for i in range(len(tx))])
+    grads = []
+    G = [0]
+    # L = np.linalg.norm(tx, 'fro')**2 + lamb <- regularizer param
+    # l_max (below) is max l for each row
+    step_size = 1 / (np.max([np.linalg.norm(tx[i])**2 for i in range(len(tx))]) + lamb)
 
-    for k in range(K - 1):
-        k_grad = log_reg_gradient(y, tx, w[k])
-
-        x = [w[k]]
-        G = [0]
-        for t in range(1, m + 1):
-            i_t = np.random.choice(np.arange(len(y)))
-            g_t = stochastic_gradient(y, tx, x[t - 1], i_t) - stochastic_gradient(y, tx, w[k], i_t) + k_grad
-            G_t = G[t - 1] + np.linalg.norm(g_t)**2
-            A_t = np.sqrt(G_t)
-
-            next_x = x[t - 1] - step_size * g_t / A_t
-            x.append(next_x)
-            G.append(G_t)
+    for k in range(max_iter):
+        i_k = np.random.choice(np.arange(len(y)))
         
-        next_w = np.mean(x)
-        w.append(next_w)
+        if k % q == 0:
+            z = w[k]
+            v = log_reg_gradient(y, tx, w[k])
 
-    return np.mean(w)
+        g_t = stochastic_gradient(y, tx, w[k], i_k) - stochastic_gradient(y, tx, z, i_k) + v
+        G_t = G[k] + np.linalg.norm(g_t)**2
+        A_t = np.sqrt(G_t)
+
+        next_w = w[k] - step_size * g_t / A_t
+
+        w.append(next_w)
+        G.append(G_t)
+        grads.append(g_t)
+
+    return grads
--- src/optimizers/SVRG.py
+++ src/optimizers/SVRG.py
@@ -4,10 +4,10 @@ from src.logistic_regression.stochastic_gradient import stochastic_gradient
 from src.utils.method_utils import *
 
 
-def SVRG(w_0, tx, y, max_iter, q):
+def SVRG(w_0, tx, y, max_iter, q, lamb):
     w = [w_0]
     grads = []
-    gamma = 1 / np.max([np.linalg.norm(tx[i])**2 for i in range(len(tx))])
+    step_size = 1 / (np.max([np.linalg.norm(tx[i])**2 for i in range(len(tx))]) + lamb)
 
     for k in range(max_iter):
         i_k = np.random.choice(np.arange(len(y)))
@@ -18,7 +18,7 @@ def SVRG(w_0, tx, y, max_iter, q):
 
         grad = stochastic_gradient(y, tx, w[k], i_k) - stochastic_gradient(y, tx, z, i_k) + v
 
-        next_w = w[k] - gamma * grad
+        next_w = w[k] - step_size * grad
 
         w.append(next_w)
         grads.append(grad)
