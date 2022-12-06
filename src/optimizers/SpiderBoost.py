0bc85bf2aa70753cc17724e9e97539aa8ba7426b
bedkowski.patrick <bedkowskisite@gmail.com>
2022-12-06 20:56:39 +0100

    Refactored method SpiderBoost to class

--- src/optimizers/SpiderBoost.py
+++ src/optimizers/SpiderBoost.py
@@ -1,82 +1,57 @@
 import numpy as np
+
 from src.logistic_regression.log_reg_gradient import log_reg_gradient
 from src.logistic_regression.stochastic_gradient import stochastic_gradient
 
-### SpiderBoost
 
-def compute_estimator_vk(tx, y, w, w_prev, prev_vk, S):
-    '''Computes gradient estimator for SpiderBoost algorithm
-    
-    Parameters
-    ----------
-    tx : ndarray of shape (N, D)
-        Array of input features
-    y : ndarray of shape (N, 1)
-        Array of output 
-    w : ndarray of shape (D, 1)
-        Current weights of the model
-    w_prev : ndarray of shape (D, 1)
-        Weights of the model from previous iteration
-    prev_v_k : ndarray of shape (D, 1)
-        Estimation of the gradient from previous iteration
-    S : ndarray of shape (D, 1)
-        Set of samples' indexes randomly chosen with replacement
+class SpiderBoost:
+    name = "SpiderBoost"
 
-    Returns
-    -------
-    v_k : ndarray of shape (D, 1)
-        Estimation of the gradient
-    '''
-    res = 0
-    for i in S:
-        partial_sum = stochastic_gradient(y, tx, w, i) - stochastic_gradient(y, tx, w_prev, i) + prev_vk
-        res = res + partial_sum
-    v_k = res/len(S)
+    def __init__(self,
+                 lipshitz_const: float):
+        """
+        Implementation of SPIDER boost method.
+        Args:
+            lipshitz_const: Lipshitz constant.
+        """
+        self.lipshitz_const = lipshitz_const
 
-    return v_k
+    def optimize(self, w_0, tx, y, max_iter):
+        """Algorithm for gradient optimization, which estimates gradients and reduces their iterative variance.
 
-def spider_boost(w_0, tx, y, max_iter, L, q, S_size):
-    """Algorithm for gradient optimization, which estimates gradients and reduces their iterative variance.
+        Parameters
+        ----------
+        w_0 : ndarray of shape (D, 1)
+            Initial weights of the model
+        tx : ndarray of shape (N, D)
+            Array of input features
+        y : ndarray of shape (N, 1)
+            Array of output
+        max_iter : int
+            Maximum number of iterations
+        Returns
+        -------
+        grads : ndarray of shape (max_iter, D)
+            Array of gradient estimators in each step of the algorithm.
+        """
+        # Intrinsic parameters initialization
+        eta = 1/(2*self.lipshitz_const)
+        N = len(tx)
+        n = len(y)
 
-    Parameters
-    ----------
-    w_0 : ndarray of shape (D, 1)
-        Initial weights of the model
-    tx : ndarray of shape (N, D)
-        Array of input features
-    y : ndarray of shape (N, 1)
-        Array of output
-    max_iter : int
-        Maximum number of iterations
-    L : float
-        Lipshitz constant.
-    q : int
-        Parameter, for which each q-th iteration standard gradient is computed.
+        # Outputs
+        grads = []
+        w = [w_0]
 
-    Returns
-    -------
-    grads : ndarray of shape (max_iter, D)
-        Array of gradient estimators in each step of the algorithm.
-    """
-    # Intrinsic parameters initialization
-    eta = 1/(2*L)
-    q = q
-    K = max_iter
-    N = len(tx)
+        # Algorithm
+        for t in max_iter:
+            if t % n == 0:
+                v_k = log_reg_gradient(tx, y, w[t]) # logistic cost function full gradient
+            else:
+                i = np.random.choice(np.arange(1, n))
+                v_k = partial_sum = stochastic_gradient(y, tx, w, i) - stochastic_gradient(y, tx, w[t-1], i) + v_k
+            w_next = w[t] - eta*v_k
+            w.append[w_next]
+            grads.append(v_k)
 
-    # Outputs
-    grads = []
-    w = [w_0]
-
-    # Algorithm
-    for k in range(K):
-        if k%q == 0:
-            v_k = log_reg_gradient(tx, y, w[k]) # logistic cost function full gradient
-        else:
-            S = np.random.choice(N, size = S_size)
-            v_k = compute_estimator_vk(tx, y, w[k], w[k-1], v_k, S)
-        w_next = w[k] - eta*v_k
-        w.append[w_next]
-        grads.append(v_k)
-    
-    return grads
+        return grads
