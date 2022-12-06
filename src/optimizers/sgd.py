2c0179bf00e49d2ba018a83b146cb5f15c3af4b7
bedkowski.patrick <bedkowskisite@gmail.com>
2022-12-06 20:56:24 +0100

    Refactored method SGD to class

--- src/optimizers/sgd.py
+++ src/optimizers/sgd.py
@@ -1,30 +1,38 @@
 import numpy as np
-from typing import List
 
-from src.logistic_regression.log_reg_gradient import log_reg_gradient
 from src.logistic_regression.stochastic_gradient import stochastic_gradient
 
 
-def sgd(w_0, tx, y, max_iter, params):
-    """
-    Compute Stochastic gradient Descent 
-    :param w_0: Initial weights vector.
-    :param max_iter: Maximum number of iterations
-    :param tx: Built model
-    :param y: Target data
-    :param params: Additional parameters for SGD. 
-    :return: List of Gradients
-    """
-    gamma = params.gamma
-    grads = []
-    w = [w_0]
-    n = len(y)
+class SGD:
+    name = "SGD"
 
-    for t in range(max_iter):
-        i_t = np.random.choice(np.arange(1, n))  # get index of sample for which to compute gradient
-        gradient = stochastic_gradient(y, tx, w[t], [i_t])
-        w_next = w[t] - gamma*gradient
+    def __init__(self,
+                 gamma: float):
+        """
+        Implementation of SGD method.
+        Args:
+            gamma: Step size
+        """
+        self.gamma = gamma
 
-        w.append(w_next)
-        grads.append(gradient)
-    return grads
+    def optimize(self, w_0, tx, y, max_iter):
+        """
+        Compute Stochastic gradient Descent
+        :param w_0: Initial weights vector.
+        :param max_iter: Maximum number of iterations
+        :param tx: Built model
+        :param y: Target data
+        :return: List of Gradients
+        """
+        grads = []
+        w = [w_0]
+        n = len(y)
+
+        for t in range(max_iter):
+            i_t = np.random.choice(np.arange(1, n))  # get index of sample for which to compute gradient
+            gradient = stochastic_gradient(y, tx, w[t], [i_t])
+            w_next = w[t] - self.gamma*gradient
+
+            w.append(w_next)
+            grads.append(gradient)
+        return grads
