9395edba2be5558ac10d0f44e84a0e539b552ccd
bedkowski.patrick <bedkowskisite@gmail.com>
2022-12-06 20:55:41 +0100

    Refactored method SVRG to class

--- src/optimizers/SVRG.py
+++ src/optimizers/SVRG.py
@@ -4,23 +4,35 @@ from src.logistic_regression.stochastic_gradient import stochastic_gradient
 from src.utils.method_utils import *
 
 
-def SVRG(w_0, tx, y, max_iter, q):
-    w = [w_0]
-    grads = []
-    gamma = 1 / np.max([np.linalg.norm(tx[i])**2 for i in range(len(tx))])
+class SVRG:
+    name = "SVRG"
 
-    for k in range(max_iter):
-        i_k = np.random.choice(np.arange(len(y)))
-        
-        if k % q == 0:
-            z = w[k]
-            v = log_reg_gradient(y, tx, w[k])
+    def __init__(self,
+                 q: float):
+        """
+        Implementation of SVRG method.
+        Args:
+            q:
+        """
+        self.q = q
 
-        grad = stochastic_gradient(y, tx, w[k], i_k) - stochastic_gradient(y, tx, z, i_k) + v
+    def optimize(self, w_0, tx, y, max_iter):
+        w = [w_0]
+        grads = []
+        gamma = 1 / np.max([np.linalg.norm(tx[i])**2 for i in range(len(tx))])
 
-        next_w = w[k] - gamma * grad
+        for k in range(max_iter):
+            i_k = np.random.choice(np.arange(len(y)))
 
-        w.append(next_w)
-        grads.append(grad)
-    
-    return grads
+            if k % self.q == 0:
+                z = w[k]
+                v = log_reg_gradient(y, tx, w[k])
+
+            grad = stochastic_gradient(y, tx, w[k], i_k) - stochastic_gradient(y, tx, z, i_k) + v
+
+            next_w = w[k] - gamma * grad
+
+            w.append(next_w)
+            grads.append(grad)
+
+        return grads
