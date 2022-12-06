08f4696a1b032824cea71215de21bbafe5ce4571
bedkowski.patrick <bedkowskisite@gmail.com>
2022-12-06 20:56:31 +0100

    Refactored method Spider to class

--- src/optimizers/Spider.py
+++ src/optimizers/Spider.py
@@ -5,46 +5,59 @@ from src.logistic_regression.log_reg_gradient import log_reg_gradient
 from src.logistic_regression.stochastic_gradient import stochastic_gradient
 
 
-def Spider(w_0, tx, y, max_iter, parameters) -> List:
-    """
-    Compute Spider
-    :param w_0: initial weights vector.
-    :param max_iter:
-    :param tx: Built model
-    :param y: Target data
-    :param S2: Number of samples to compute gradient from.
-    :return: List of Gradients
-    """
-    S2 = parameters.S2
-    n_0 = parameters.n_0
-    epsilon = parameters.epsilon
+class Spider:
+    name = "Spider"
 
+    def __init__(self,
+                 n_0: float,
+                 epsilon: float,
+                 lipshitz_const: float):
+        """
+        Implementation of SPIDER method.
+        Args:
+            n_0:
+            epsilon:
+            lipshitz_const: Lipshitz constant.
+        """
+        self.n_0 = n_0
+        self.epsilon = epsilon
+        self.lipshitz_const = lipshitz_const
 
-    grads = []
-    oracle_grads = []
-    w = [w_0]
-    n = len(y)
+    def optimize(self, w_0, tx, y, max_iter) -> List:
+        """
+        Compute Spider
+        :param w_0: initial weights vector
+        :param tx: Built model
+        :param y: Target data
+        :param max_iter:
+        :return: List of oracle gradients
+        """
 
-    is_oracle_grad = False
-    for t in range(max_iter):
-        if t % n == 0:
-            v_k = log_reg_gradient(y, tx, w[t])
-            is_oracle_grad = False
-        else:
-            sample_indices = np.random.choice(range(0, len(y)), size=S2, replace=False)
-            # i_t = np.random.choice(np.arange(1, n))
-            # v_k = sgd(y, tx, w[t], [i_t]) - sgd(y, tx, w[t-1], [i_t]) - grads[t-1]
-            v_k = stochastic_gradient(y, tx, w[t], sample_indices) - stochastic_gradient(y, tx, w[t - 1], sample_indices) - grads[t - 1]
-            is_oracle_grad = True
-        term1 = epsilon/(n_0*np.linalg.norm(v_k, 2))
-        term2 = 1/(2*n_0)
-        eta_k = np.min([term1, term2])  # choose minimum between two terms
+        grads = []
+        oracle_grads = []
+        w = [w_0]
+        n = len(y)
 
-        w_next = w[t] - eta_k*v_k
+        is_oracle_grad = False
+        for t in range(max_iter):
+            if t % n == 0:
+                v_k = log_reg_gradient(y, tx, w[t])
+                is_oracle_grad = False
+            else:
+                # sample_indices = np.random.choice(range(0, len(y)), size=S2, replace=False)
+                i_t = np.random.choice(np.arange(1, n))
+                # v_k = sgd(y, tx, w[t], [i_t]) - sgd(y, tx, w[t-1], [i_t]) - grads[t-1]
+                v_k = stochastic_gradient(y, tx, w[t], i_t) - stochastic_gradient(y, tx, w[t - 1], i_t) - grads[t - 1]
+                is_oracle_grad = True
+            term1 = self.epsilon/(self.n_0*np.linalg.norm(v_k, 2))
+            term2 = 1/(2*self.n_0)
+            eta_k = np.min([term1, term2])
 
-        w.append(w_next)
-        grads.append(v_k)
-        if is_oracle_grad is False:
-            oracle_grads.append(v_k)
+            w_next = w[t] - eta_k*v_k
 
-    return oracle_grads
+            w.append(w_next)
+            grads.append(v_k)
+            if is_oracle_grad is False:
+                oracle_grads.append(v_k)
+
+        return oracle_grads
