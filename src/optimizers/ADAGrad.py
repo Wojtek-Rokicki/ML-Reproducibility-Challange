93396307bbbcd8ae955091244a0994384aa2916b
bedkowski.patrick <bedkowskisite@gmail.com>
2022-12-06 20:55:52 +0100

    Refactored method AdaGrad to class

--- src/optimizers/ADAGrad.py
+++ src/optimizers/ADAGrad.py
@@ -1,49 +1,55 @@
 import numpy as np
 from src.logistic_regression.log_reg_gradient import log_reg_gradient
 
-### AdaGrad
 
-def AdaGrad(w_0, tx, y, max_iter, lambda_, epsilon = 1e-8):
-    '''Algoritm for adaptive gradient optimization.
+class AdaGrad:
+    name = "AdaGrad"
 
-    Adapts learing parameter - smaller rate for frequent features (well-suited for sparse data). 
+    def __init__(self,
+                 lambda_: float,
+                 epsilon: float = 1e-8):
+        """
+        Implementation of AdaGrad method.
+        Args:
+            lambda_:
+            epsilon:
+        """
+        self.lambda_ = lambda_
+        self.epsilon = epsilon
 
-    Parameters
-    ----------
-    w_0 : ndarray of shape (D, 1)
-        Initial weights of the model
-    tx : ndarray of shape (N, D)
-        Array of input features
-    y : ndarray of shape (N, 1)
-        Array of output
-    max_iter : int
-        Maximum number of iterations
-    lambda_ : float
-        Initial learning rate.
-    epsilon : float
-        Smoothing term, which avoids dividing by zero.
+    def optimize(self, w_0, tx, y, max_iter):
+        '''Algoritm for adaptive gradient optimization.
 
-    Returns
-    -------
-    grads : ndarray of shape (max_iter, D)
-        Array of gradient estimators in each step of the algorithm.
-    '''
-    K = max_iter
-    lambda_ = lambda_
-    epsilon = epsilon
-    D = len(w_0)
-    G_t = np.zeros((D,D))
-    
-    # Outputs
-    grads = []
-    w = [w_0]
+        Adapts learing parameter - smaller rate for frequent features (well-suited for sparse data).
 
-    for k in range(K):
-        g_t = log_reg_gradient(tx, y, w[k])
-        G_t += np.diag([g_t_i ** 2 for g_t_i in g_t])
-        v_k = lambda_ / np.sqrt(G_t + epsilon) * g_t
-        w_next = w[k] - v_k
-        w.append(w_next)
-        grads.append(v_k)
+        Parameters
+        ----------
+        w_0 : ndarray of shape (D, 1)
+            Initial weights of the model
+        tx : ndarray of shape (N, D)
+            Array of input features
+        y : ndarray of shape (N, 1)
+            Array of output
+        max_iter : int
+            Maximum number of iterations
+        Returns
+        -------
+        grads : ndarray of shape (max_iter, D)
+            Array of gradient estimators in each step of the algorithm.
+        '''
+        D = len(w_0)
+        G_t = np.zeros((D, D))
 
-    return grads
+        # Outputs
+        grads = []
+        w = [w_0]
+
+        for t in range(max_iter):
+            g_t = log_reg_gradient(tx, y, w[t])
+            G_t += np.diag([g_t_i ** 2 for g_t_i in g_t])
+            v_k = self.lambda_ / np.sqrt(G_t + self.epsilon) * g_t
+            w_next = w[t] - v_k
+            w.append(w_next)
+            grads.append(v_k)
+
+        return grads
